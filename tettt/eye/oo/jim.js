5330211855179955686c4f66e1c8469452765118
Pritam2017331076 <79059787+Pritam2017331076@users.noreply.github.com>
2021-04-28 00:39:06 +0600

    Create jim.js

---
+++ tettt/eye/oo/jim.js
@@ -0,0 +1,12 @@
+/*
+ * Copyright IBM Corp. All Rights Reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict';
+
+const kenakata = require('./lib/kenakata');
+
+module.exports.Kenakata = kenakata;
+module.exports.contracts = [kenakata];
